e8d9678e2c1f28f34d06f24049e1bf396bd5cb92
surya8barca <surya8barca@gmail.com>
2020-08-15 12:44:39 +0530

    home

---
+++ Surya/android_app/lib/home.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+        title: Text(
+          'Pandemic',
+          style: TextStyle(
+            fontSize: 28.0, //media query based later
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Builder(
+        builder: (context) => SingleChildScrollView(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
